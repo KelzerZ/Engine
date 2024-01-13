69ecc9c254456e6f4de9c49f572dd35075b18532
kelzerz <kelzerkelzer350@gmail.com>
2024-01-13 21:03:59 +0300

    Created Render

---
+++ render/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require("path");
+
+module.exports = {
+	entry: "./src/index.ts",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
+	},
+	resolve: {
+		extensions: [".ts"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src/index.html"),
+			filename: "index.html",
+			inject: "body"
+		}),
+		new MiniCssExtractPlugin({
+			filename: "styles.css"
+		})
+	]
+};
